5ebeff439583a48acac50ed1868776df7fb56a6f
Shreyas <sonureigns007@gmail.com>
2024-01-23 16:54:42 +0530

    importing and init socket

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const { createServer } = require("node:http");
 const { join } = require("node:path");
+const { Server } = require("socket.io");
 
 const app = express();
 const server = createServer(app);
+const io = new Server(server);
 
 app.get("/", (req, res) => {
   res.sendFile(join(__dirname, "index.html"));
+});
+
+io.on("connection", socket => {
+  console.log("a user connected");
 });
 
 server.listen(3000, () => {
