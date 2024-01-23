ec9aaf73d1a8f56a81ae0669b294202b35d1e53e
Shreyas <sonureigns007@gmail.com>
2024-01-23 16:56:48 +0530

    emitting a event

--- index.js
+++ index.js
@@ -12,7 +12,9 @@ app.get("/", (req, res) => {
 });
 
 io.on("connection", socket => {
-  console.log("a user connected");
+  socket.on("chat message", msg => {
+    console.log("message: " + msg);
+  });
 });
 
 server.listen(3000, () => {
